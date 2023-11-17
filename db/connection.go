5a7c8a3a0f3b96a39718025593809b4d5cace193
Devansh Mishra <mdevansh28@gmail.com>
2023-11-17 13:46:10 +0530

    Basic DB connection

--- db/connection.go
+++ db/connection.go
@@ -1,8 +1,18 @@
 package db
 
-import(
+import (
 	"context"
-	
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func ConnectDB() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI("mongodb://username:password@localhost:27017/?authSource=mydatabase")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
