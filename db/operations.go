53f9de658c2b00cd343df2f6d460e508943d1f18
Devansh Mishra <mdevansh28@gmail.com>
2023-11-18 15:01:39 +0530

    Create user fucntionality basic

--- db/operations.go
+++ db/operations.go
@@ -2,11 +2,12 @@ package db
 
 import (
 	"context"
+	"security-go/models"
 
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func CreateUser(collection *mongo.Collection, user interface{}) error {
+func CreateUser(collection *mongo.Collection, user models.User) error {
 	_, err := collection.InsertOne(context.Background(), user)
 	return err
 }
