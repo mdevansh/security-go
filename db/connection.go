dd7b1e6481b169cee301af7681d5af7f6680c46a
Devansh Mishra <mdevansh28@gmail.com>
2023-11-18 15:01:19 +0530

    Initialize DB connection

--- db/connection.go
+++ db/connection.go
@@ -2,13 +2,16 @@ package db
 
 import (
 	"context"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var mongoUri = os.Getenv("MONGO_URI")
+
 func ConnectDB() (*mongo.Client, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://username:password@localhost:27017/?authSource=mydatabase")
+	clientOptions := options.Client().ApplyURI(mongoUri)
 	client, err := mongo.Connect(context.Background(), clientOptions)
 
 	if err != nil {
