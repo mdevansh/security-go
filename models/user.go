b666d6fb3c03eb0e54770e0a367dc86d7f2e38ac
Devansh Mishra <mdevansh28@gmail.com>
2023-11-18 15:11:54 +0530

    Mandated necessary fields

--- models/user.go
+++ models/user.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type User struct {
-	Username  string    `json:"username" bson:"_id"`
-	Password  string    `json:"password" bson:"password"`
-	Roles     []string  `json:"roles" bson:"roles"`
+	Username  string    `json:"username" bson:"_id" binding:"required"`
+	Password  string    `json:"password" bson:"password" binding:"required"`
+	Roles     []string  `json:"roles" bson:"roles" binding:"required"`
 	LastLogin time.Time `json:"lastLogin" bson:"lastLogin"`
 	IsActive  bool      `json:"isActive" bson:"isActive"`
 }
